660d40e01d68657237f10d8e69ee06ad39c555e0
Feilueze ht <feilueze@163.com>
2022-09-20 07:06:14 +0000

    删除  kafeimall代码

--- kafeiMall/kafeimall-common/src/main/java/com/kafeimall/common/domain/AggregateRoot.java
+++
@@ -1,9 +0,0 @@
-package com.kafeimall.common.domain;
-
-/**
- * @author: zzg
- * @date: 9/1/22
- * @Description: 聚合根抽象类（后续作扩展）
- */
-public abstract class AggregateRoot<T> {
-}
--- kafeiMall/kafeimall-common/src/main/java/com/kafeimall/common/valid/UpdateGroup.java
+++
@@ -1,9 +0,0 @@
-package com.kafeimall.common.valid;
-
-/**
- * @author: zzg
- * @date: 8/22/22
- * @Description: 修改校验组
- */
-public interface UpdateGroup {
-}
